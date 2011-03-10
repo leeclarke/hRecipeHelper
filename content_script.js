3c4a080a52e03ac365cf06c75080b3cce7fdca94
Lee Clarke <lee.k.clarke@gmail.com>
2011-03-09 19:56:19 -0500

    celaned up code a bit

--- content_script.js
+++ content_script.js
@@ -3,7 +3,6 @@
  */
 function updateText(textIn) {
 	console.log("textIn=="+textIn);
-	//document.getElementById('postingHtmlBox').innerHTML += "<p>Droid</p>"
 	if(document.activeElement.type == 'textarea') {
 		document.activeElement.innerHTML += textIn
 	} else if(document.activeElement.type = 'text') {
