bef20940e4a0de2e2fb633ef1872f8ad055b4b4b
Lee Clarke <lee.k.clarke@gmail.com>
2011-03-09 19:56:22 -0500

    celaned up code a bit

--- content_script.js
+++ content_script.js
@@ -2,7 +2,6 @@
  * Inserts the text into the current field.
  */
 function updateText(textIn) {
-	console.log("textIn=="+textIn);
 	if(document.activeElement.type == 'textarea') {
 		document.activeElement.innerHTML += textIn
 	} else if(document.activeElement.type = 'text') {
