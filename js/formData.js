bc4382686b3465f55ca6b9bc5ab36616a70d183d
Lee <lee.k.clarke@gmail.com>
2011-03-14 11:06:05 -0400

    Refactored all Data related methods out of Form related code.

--- js/formData.js
+++ js/formData.js
@@ -1,5 +1,7 @@
+var debugEnabled = true;
+
 /**
- *
+ * Object representing the Form Data used to generate the hRecipe code.
  */
 function FormData() {
 	this.$recName = '';
@@ -18,12 +20,39 @@ function FormData() {
 	this.$tags = [];
 }
 
-// Little debug method to keep it all clean 
-var debugEnabled = true;
-
+/**
+ * Little debug method to keep code clean and able to turn off logging for deploy. 
+ */
 function debug(msg) {
 	if(debugEnabled) {
 		console.log(msg);
 	}
 }
 
+/**
+ * Retrieves form data from localStorage.
+ */
+function loadFormData() {
+	var objStr = localStorage["hRecipeData"];
+	debug("loadedData == "+objStr);
+	var formDataParse;
+	if(objStr && objStr.length >2){
+		formDataParse = jQuery.parseJSON(objStr);
+		debug("formDataParse == "+formDataParse + " name==" + formDataParse.$recName);
+	}
+	return formDataParse;
+}
+
+/**
+ * Write form data to localStorage as Json string.
+ */
+function saveFormData(formData) {
+	localStorage["hRecipeData"] = JSON.stringify(formData);
+}
+
+/**
+ * Removes form data from localStorage.
+ */
+function deleteFormData() {
+	localStorage["hRecipeData"] = "";
+}
